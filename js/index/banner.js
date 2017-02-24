3d5d3992096b97766dae6d581a9b47d7d671d215
yhy <920430533@qq.com>
2017-02-24 17:47:11 +0800

    banner模块完成

--- js/index/banner.js
+++ js/index/banner.js
@@ -3,30 +3,95 @@ define(['jquery'],function($){
 		init:function(){
 			$('.banner').load("../html/index/banner.html",function(){
 				//轮播图
-				function Carousel(eles,line,next,pre){
+				function Carousel(eles,line,next,prev,texts){
 					//自动播放
 					this.autoplay(eles,line);
+					// this.timer(this.play,3000);
+					//绑定事件
+					this.bind(eles,line,next,prev,texts);
 				}
 				//轮播图的索引值
 				Carousel.prototype.index = 0;
+				Carousel.prototype.play = function(eles,line){
+					this.index++;
+					this.index = this.index % eles.length;
+					$('.banner_pic').eq(this.index%3).stop().animate({opacity:1},1000)
+					 .siblings('li').stop().animate({'opacity':0},1000);
+					 var offLeft = this.index * $(line).width();
+					$(line).animate({
+						'left':offLeft
+					})
+				}
 				//自动播放
 				Carousel.prototype.autoplay = function(eles,line){
 					var self = this;
 					this.timer = setInterval(function(){
-						self.index++;
-						for(var i=0;i < eles.length;i++){
-							eles[i].style.opacity = 0;
-						}
-						self.index = self.index % eles.length;
-						//下划线运动
+						 self.index++;
+						 self.index = self.index % eles.length;
+						 $('.banner_pic').eq(self.index % eles.length).stop().animate({opacity:1},1000)
+						  .siblings('li').stop().animate({'opacity':0},1000);
+						  var offLeft = self.index * $(line).width();
 						$(line).animate({
-
+							'left':offLeft
 						})
-						line[self.index].className = 'banner_num_active';
-						starmove(self.ele[self.i],{"opacity":100});
 					},3000);
-
 				}
+				// Carousel.prototype.timer = setInterval;
+				Carousel.prototype.bind = function(eles,line,next,prev,texts){
+					var self = this;
+					// console.log(self.play);
+					$(eles).parent('ul').hover(function(){
+						$(prev).animate({
+							left:0,
+							opacity:1
+						});
+						$(next).animate({
+							right:0,
+							opacity:1
+						})
+					},function(){
+						$(prev).animate({
+							opacity:0,
+							left:-45
+						});
+						$(next).animate({
+							opacity:0,
+							right:-45
+						})
+					})
+				
+					$(texts).mouseover(function(){
+						console.log($(this).index());
+						clearInterval(self.timer);
+						self.index = $(this).index() - 1;
+						// console.log(self);
+						self.play(eles,line);
+						// console.log(self.index);
+					})
+					$(texts).mouseleave(function(){
+						self.autoplay(eles,line);
+					})
+					$(next).click(function(){
+						clearInterval(self.timer);
+						self.play(eles,line);
+						self.autoplay(eles,line);
+					})
+					$(prev).click(function(){
+						clearInterval(self.timer);
+						if(self.index == 0){
+							self.index = eles.length;
+						}
+						self.index = self.index - 2;
+						self.play(eles,line);
+						self.autoplay(eles,line);
+					})
+				}
+				var aList = document.getElementsByClassName('banner_pic');
+				var line = document.getElementsByClassName('banner_line')[0];
+				var prev = document.getElementsByClassName('banner_btn_pre')[0];
+				var next = document.getElementsByClassName('banner_btn_next')[0];
+				var text = document.getElementsByClassName('banner_text');
+				var banner = new Carousel(aList,line,next,prev,text);
 			})
 		}
 		
---
+++ php/index/banner.php
@@ -0,0 +1,44 @@
+<?php 
+    class Menu{
+        public $name;
+    }
+    
+    $con = new mysqli('127.0.0.1','root','','vip');
+    $con->query("set names utf8"); //设置编码为utf8 显示中文
+    $sql = 'select * from menu_list';
+    $res = $con->query($sql);
+    $arr = array();
+    if(!$con->connect_error){
+         if($res->num_rows > 0){
+                while($row = $res->fetch_assoc()){
+                    $menu = new Menu();
+                    $menu->name = $row["name"];
+                    array_push($arr, $menu);
+                }
+            }
+    }else{
+        array_push($arr, array('status' => 100, 'msg'=>"连接数据库失败"));
+    }
+	//返回json字符串
+    // print_r(json_encode($arr));
+    
+        //判断是否是jsonp的请求
+        //isset(var) 判断变量是否被定义
+        if(isset($_REQUEST['callback'])){
+            // 表示是一个jsonp的请求
+            //找到callback 的函数名称
+            $callback = $_REQUEST['callback'];
+            //调用callback的函数
+            $str = json_encode($arr);
+            print_r( $callback."('$str')");
+        }else{
+            array_push($arr, "跨域失败");
+            $str = json_encode($arr);
+            print_r( $str );
+
+        }
+
+
+  //  print_r($str)
+
+ ?>
